601b67300e643aeee4224716a3d0909291e77b08
Mayank Prasoon <mayank.prasoon@outlook.com>
2022-04-20 08:51:08 +0530

    Added details wrt contract compilation

--- src/backend/test/NFTProjectMarketplace.test.js
+++ src/backend/test/NFTProjectMarketplace.test.js
@@ -6,6 +6,7 @@ const fromWei = (num) => ethers.utils.formatEther(num)
 describe("NFTProjectMarketplace", function() {
   let deployer, addr1, addr2, nft, nftprojectmarket, addrs;
   let feePercent = 1;
+  let sellTimeDelay = 600000; // 10 mins
   let testURI = "Sample URI";
 
   beforeEach(async function () {
@@ -15,6 +16,18 @@ describe("NFTProjectMarketplace", function() {
       [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
 
       nft = await NFT.deploy()
-      nftprojectmarket = await NFTProjectMarket.deploy()
+      nftprojectmarket = await NFTProjectMarket.deploy(feePercent, sellTimeDelay)
+  })
+
+  describe("Deployment", function () {
+    it("Should track symbol of nft collection", async function () {
+      expect(await nft.name()).to.equal("Art timeline NFT")
+      expect(await nft.symbol()).to.equal("ARTN")
+    })
+    it("Should track details of the marketplace collection", async function () {
+      expect(await nftprojectmarket.feeAccount()).to.equal(deployer.address)
+      expect(await nftprojectmarket.feePercent()).to.equal(feePercent)
+      expect(await nftprojectmarket.sellTimeDelay()).to.equal(sellTimeDelay)
+    })
   })
 })
